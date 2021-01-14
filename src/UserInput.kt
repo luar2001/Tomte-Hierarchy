e2d1094459cb53936840a7719a5df34d5a6cdefe
Luar2001 <Hagtorn30>
2021-01-14 17:22:22 +0100

    Created 2 classes.main.java that starts the program and UserInput.kt that takes care of user input.

---
+++ src/UserInput.kt
@@ -0,0 +1,36 @@
+/**
+Created by Lukas Aronsson
+Date: 14/01/2021
+Time: 16:52
+Project: IndividuelUppgift2
+Copyright: MIT
+ **/
+
+/**
+ * Object that asks for user input and sends a message that tells the user to write an input
+ * @param thing a String that tells the user what the program wants inputted
+ */
+class UserInput(thing: String) {
+
+    /**
+     * constructor that starts with a message() then a input()
+     */
+    init {
+        message(thing)
+        input()
+    }
+
+    /**
+     * prints a message that tells the user what the program wants it to input
+     */
+    private fun message(thing: String) {
+        print("Input $thing here: ") //returns a string
+    }
+
+    /**
+     * Reads user input and returns it in lowercase along as its not null
+     */
+    private fun input(): String {
+        return readLine()!!.toLowerCase() //Reads user input that is not null and forces it to be in lowercase
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Name Here
+ * Date: 14/01/2021
+ * Time: 13:59
+ * Project: IndividuelUppgift2
+ * Copyright: MIT
+ **/
+public class main {
+    public static void main(String[] args) {
+        System.out.println(new UserInput("Name"));
+
+
+    }
+}
