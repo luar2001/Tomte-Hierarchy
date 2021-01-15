709607b9948180aefca2e489c9045417166a4a48
Luar2001 <Hagtorn30>
2021-01-15 15:19:46 +0100

    Changed the test if statement to a ternary operator inside of a printline. (to check if the user typed in a propriet direction which then will be answerd acordingly)

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,27 @@
 import java.util.Arrays;
 import java.util.List;
 
+/*
+  Created by Lukas Aronsson
+  Date: 14/01/2021
+  Time: 13:59
+  Project: Tomte-Hierarchy
+  Copyright: MIT
+ */
+
 /**
- * Created by Lukas Aronsson
- * Date: 14/01/2021
- * Time: 13:59
- * Project: Tomte-Hierarchy
- * Copyright: MIT
- **/
+ * Main class that starts the program and calls all the other classes.
+ */
 public class Main {
     public static void main(String[] args) {
-
-        List<String> directions = Arrays.asList("higher", "lower");
+        List<String> directions = Arrays.asList("higher", "lower"); //list that holds the 2 directions the user can check in the hierarchy of Tomtar
 
         UserInput name = new UserInput("name");
         UserInput direction = new UserInput("direction");
 
-        if (direction.inList(directions)) {
-            System.out.println(direction.getInput() + " is on the list");
-        } else {
-            System.out.println(direction.getInput() + " is not on the list");
-        }
-
         System.out.println(name.getInput());
+
+        System.out.println(direction.getInput() + " is " + ((direction.inList(directions)) ? "" : "not ") + "on the list");
 
     }
 }
