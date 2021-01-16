29684028f98055d999a92daaa472933824559321
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-01-16 22:33:04 +0100

    Created the tomte Class and made a untested verstion of the recurtion cycle for gattering the bosses of the tomtar into a list. .

--- src/Tomte.java
+++ src/Tomte.java
@@ -6,8 +6,61 @@
  * Copyright: MIT
  */
 
+import java.util.List;
+
 /**
  *
  */
 public class Tomte {
+    String name = null; //name of the Tomte
+    Tomte boss = null; //Tomtens boss
+
+    List<Tomte> workers = null; //list of the Tomtar that is directly under this tomte.
+
+    List<Tomte> HigherRanked = null;
+    List<Tomte> LowerRanked = null;
+
+    public Tomte(String name, Tomte boss, List<Tomte> workers) {
+        setName(name);
+        setBoss(boss);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Tomte getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Tomte boss) {
+        this.boss = boss;
+    }
+
+    public List<Tomte> getWorkers() {
+        return workers;
+    }
+
+    public void Hierarchy(String direction) {
+
+
+    }
+
+    public List<Tomte> makeHigherHierarchyList() {
+        while (!(boss == null)) {
+            HigherRanked.add(getBoss());
+            boss.makeHigherHierarchyList();
+        }
+        return HigherRanked;
+    }
+
+    public void makeLowerHierarchyList() {
+        LowerRanked.addAll(workers);
+        makeLowerHierarchyList(); //Workers here ?
+
+    }
 }
