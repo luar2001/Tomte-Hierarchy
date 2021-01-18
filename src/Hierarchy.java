ea3e97d2c3b9a5f6b1eb49d8550bb6b64ea8640f
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-01-18 20:52:58 +0100

    Cleaned, commented and reformatted the Hierarchy.java class (the makeLowerHierarchyList method is still not functional)

--- src/Hierarchy.java
+++ src/Hierarchy.java
@@ -8,12 +8,20 @@ import java.util.stream.Collectors;
  * Time: 16:27
  * Project: Tomte-Hierarchy
  * Copyright: MIT
+ * <p>
+ * Class that takes in a name and checks if it correlates to one of the Tomtar and then sends a tomte back.
  **/
 public class Hierarchy {
-
     private static final List<Tomte> higherRanked = new ArrayList<>();
     private static final List<Tomte> lowerRanked = new ArrayList<>();
 
+    /**
+     * Method that takes in a direction and then a tomte to start the hierarchy from then returns a list of the hierarchy in the inputted direction
+     *
+     * @param direction Higher = UP in the hierarchy || Lower = DOWN in the hierarchy
+     * @param tomte     a Tomte object that refers to one of the Tomtar
+     * @return A list of all the names in the hierarchy up or down depending on the direction from the inputted tomte based on the direction
+     */
     public static List<String> hierarchyCreator(String direction, Tomte tomte) {
         if (direction.equals("higher")) {
             makeHigherHierarchyList(tomte);
@@ -28,12 +36,23 @@ public class Hierarchy {
         }
     }
 
+    /**
+     * Gets all the Tomte names and puts them in a list it then returns
+     *
+     * @param tomtar a list of Tomte objects
+     * @return a list of strings that represents the Tomte objects names.
+     */
     public static List<String> getNames(List<Tomte> tomtar) {
         return tomtar.stream()
                 .map(Tomte::getName)
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Makes the hierarchy in the upwards direction form the tomte it takes in.
+     *
+     * @param tomte a Tomte object
+     */
     public static void makeHigherHierarchyList(Tomte tomte) {
         higherRanked.add(tomte.getBoss());
         if (!tomte.getBoss().equals(Tomtar.tomten)) {
@@ -41,6 +60,11 @@ public class Hierarchy {
         }
     }
 
+    /**
+     * Makes the hierarchy in the downwards direction form the tomte it takes in.
+     *
+     * @param tomte a Tomte object
+     */
     public static List<Tomte> makeLowerHierarchyList(Tomte tomte) {
         while (true) {
             lowerRanked.addAll(tomte.getWorkers());
