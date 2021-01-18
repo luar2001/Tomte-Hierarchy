b37f05c1c097856cf8bffe63fa88e87c821158bd
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-01-18 22:37:57 +0100

    Added workers to the Tomtar by adding a bunch of lists and a method called setWorkers.

--- src/Tomtar.java
+++ src/Tomtar.java
@@ -7,6 +7,7 @@
  */
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,42 +17,59 @@ import java.util.stream.Collectors;
  */
 public class Tomtar {
 
+    private static final List<Tomte> workersTroger = Collections.singletonList(null);
+    private static final List<Tomte> workersBlyger = Collections.singletonList(null);
+    private static final List<Tomte> workersRadjuret = Collections.singletonList(null);
+    private static final List<Tomte> workersNyckelpigan = Collections.singletonList(null);
+    private static final List<Tomte> workersHaren = Collections.singletonList(null);
+    private static final List<Tomte> workersGrasuggan = Collections.singletonList(null);
+    private static final List<Tomte> workersDammrattan = Collections.singletonList(null);
+    private static final List<Tomte> workersBladlusen = Collections.singletonList(null);
+    public static Tomte tomten = new Tomte("Tomten", null);
+    public static Tomte glader = new Tomte("Glader", tomten);
+    public static Tomte butter = new Tomte("Butter", tomten);
+    private static final List<Tomte> workersTomten = Arrays.asList(glader, butter);
+    public static Tomte troger = new Tomte("Tröger", glader);
+    public static Tomte blyger = new Tomte("Blyger", glader);
+    public static Tomte trotter = new Tomte("Trötter", glader);
+    private static final List<Tomte> workersGlader = Arrays.asList(troger, blyger, trotter);
+    public static Tomte radjuret = new Tomte("Rådjuret", butter);
+    public static Tomte nyckelpigan = new Tomte("Nyckelpigan", butter);
+    public static Tomte raven = new Tomte("Räven", butter);
+    public static Tomte haren = new Tomte("Haren", butter);
+    private static final List<Tomte> workersButter = Arrays.asList(radjuret, nyckelpigan, raven, haren);
+    public static Tomte skumtomten = new Tomte("Skumtomten", trotter);
+    private static final List<Tomte> workersTrotter = Collections.singletonList(skumtomten);
+    public static Tomte grasuggan = new Tomte("Gråsuggan", raven);
+    public static Tomte myran = new Tomte("Myran", raven);
+    public static Tomte dammrattan = new Tomte("Dammråttan", skumtomten);
+    private static final List<Tomte> workersSkumtomten = Collections.singletonList(dammrattan);
+    public static Tomte bladlusen = new Tomte("Bladlusen", myran);
+    private static final List<Tomte> workersRaven = Arrays.asList(grasuggan, myran, bladlusen);
+    private static final List<Tomte> workersMyran = Collections.singletonList(bladlusen);
+    public static List<Tomte> listOfTomtar = Arrays.asList(tomten, glader, butter, troger, blyger, trotter, radjuret, nyckelpigan, raven, haren, skumtomten, grasuggan, myran, dammrattan, bladlusen);
 
-        /*
-        List<Tomte> workersTomten;
-        List<Tomte> workersGlader;
-        List<Tomte> workersButter;
-        List<Tomte> workersTroger;
-        List<Tomte> workersBlyger;
-        List<Tomte> workersTrotter;
-        List<Tomte> workersRadjuret;
-        List<Tomte> workersNyckelpigan;
-        List<Tomte> workersRaven;
-        List<Tomte> workersHaren;
-        List<Tomte> workersSkumtomten;
-        List<Tomte> workersGrasuggan;
-        List<Tomte> workersMyran;
-        List<Tomte> workersDammrattan;
-        List<Tomte> workersBladlusen;
-        */
+    /**
+     * Method that sets the Tomtars Workers aka the tomtar directly under the tomte in the Hierarchy
+     */
+    public static void setWorkers() {
+        tomten.setWorkers(workersTomten);
+        glader.setWorkers(workersGlader);
+        butter.setWorkers(workersButter);
+        troger.setWorkers(workersTroger);
+        blyger.setWorkers(workersBlyger);
+        trotter.setWorkers(workersTrotter);
+        radjuret.setWorkers(workersRadjuret);
+        nyckelpigan.setWorkers(workersNyckelpigan);
+        raven.setWorkers(workersRaven);
+        haren.setWorkers(workersHaren);
+        skumtomten.setWorkers(workersSkumtomten);
+        grasuggan.setWorkers(workersGrasuggan);
+        myran.setWorkers(workersMyran);
+        dammrattan.setWorkers(workersDammrattan);
+        bladlusen.setWorkers(workersBladlusen);
 
-    static Tomte tomten = new Tomte("Tomten", null);
-    static Tomte glader = new Tomte("Glader", tomten);
-    static Tomte butter = new Tomte("Butter", tomten);
-    static Tomte troger = new Tomte("Tröger", glader);
-    static Tomte blyger = new Tomte("Blyger", glader);
-    static Tomte trotter = new Tomte("Trötter", glader);
-    static Tomte radjuret = new Tomte("Rådjuret", butter);
-    static Tomte nyckelpigan = new Tomte("Nyckelpigan", butter);
-    static Tomte raven = new Tomte("Räven", butter);
-    static Tomte haren = new Tomte("Haren", butter);
-    static Tomte skumtomten = new Tomte("Skumtomten", trotter);
-    static Tomte grasuggan = new Tomte("Gråsuggan", raven);
-    static Tomte myran = new Tomte("Myran", raven);
-    static Tomte dammrattan = new Tomte("Dammråttan", skumtomten);
-    static Tomte bladlusen = new Tomte("Bladlusen", myran);
-
-    static List<Tomte> listOfTomtar = Arrays.asList(tomten, glader, butter, troger, blyger, trotter, radjuret, nyckelpigan, raven, haren, skumtomten, grasuggan, myran, dammrattan, bladlusen);
+    }
 
     /**
      * Checks if the name parameter is equal to any of the names of the tomtar and then returns a list of them.
