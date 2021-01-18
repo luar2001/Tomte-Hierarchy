19186731bb90af80b87b0286700ff581c206ed40
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-01-18 21:26:35 +0100

    Working and semi cleaned Main.java (if you write the wrong name you get error message)

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,36 @@
 import java.util.Arrays;
 import java.util.List;
-
-/*
-  Created by Lukas Aronsson
-  Date: 14/01/2021
-  Time: 13:59
-  Project: Tomte-Hierarchy
-  Copyright: MIT
- */
+import java.util.Objects;
 
 /**
+ * Created by Lukas Aronsson
+ * Date: 14/01/2021
+ * Time: 13:59
+ * Project: Tomte-Hierarchy
+ * Copyright: MIT
+ * <p>
  * Main class that starts the program and calls all the other classes.
- */
+ **/
 public class Main {
     public static void main(String[] args) {
         List<String> directions = Arrays.asList("higher", "lower"); //list that holds the 2 directions the user can check in the hierarchy of Tomtar
-        List<String> names = Arrays.asList("Tomten")
+
+        System.out.println("First Input Name || Then Input a Direction (Higher or Lower) ");
 
         UserInput name = new UserInput("name");
         UserInput direction = new UserInput("direction");
+        System.out.println(direction.getInput() + " is " + ((direction.inList(directions)) ? "" : "not ") + "on the list of valid directions");
 
-        if (name.inList(names)) {
+        Tomte selectedTomte = Tomtar.selectedTomte((name.getInput()));
 
-
+        if (Objects.equals(Objects.requireNonNull(direction.getInput()).toLowerCase(), "higher") && selectedTomte.getBoss() == null || Objects.equals(Objects.requireNonNull(direction.getInput()).toLowerCase(), "lower") && selectedTomte.getWorkers() == null) {
+            System.out.println(selectedTomte.getName() + " is the end of the Hierarchy");
         } else {
-            System.out.println("Input a name of a Tomte: ");
+            if (direction.inList(directions)) {
+                System.out.println(Hierarchy.hierarchyCreator(Objects.requireNonNull(direction.getInput()), selectedTomte));
+            } else {
+                System.out.println("Please try again :) ");
+            }
         }
-
-        System.out.println(direction.getInput() + " is " + ((direction.inList(directions)) ? "" : "not ") + "on the list");
-
-
     }
 }
